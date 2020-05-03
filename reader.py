59555edf7ffdf8fd992128e5d50e510a4f99a346
Strugari, Stefan <Stefan.Strugari@centric.eu>
2020-05-03 05:07:01 +0300

    Add some problem instances files and a file reader for them

---
+++ reader.py
@@ -0,0 +1,46 @@
+class Graph :
+    def __init__(self, vertex_count: int, edge_count: int, edges: list) :
+        if vertex_count <= 0 :
+            raise Exception("Invalid vertex count")
+
+        if edge_count < 0 and len(edges) != edge_count :
+            raise Exception("Invalid edge count")
+
+        self.vertex_count = vertex_count
+        self.edge_count = edge_count
+        self.edges = edges
+    def __repr__(self) -> str:
+        return str(self.__dict__)
+
+def read_instance(file_path: str) -> Graph :
+    f = open(file_path, 'r') 
+
+    edges = []
+    while True: 
+        line = f.readline()
+  
+        if not line: 
+            break
+
+        if line.find("p") == 0 :
+            vertex_count = int(line.split()[2].strip())
+            edge_count = int(line.split()[3].strip())
+        elif line.find("e") == 0 :
+            v1 = int(line.split(' ')[1].strip())
+            v2 = int(line.split(' ')[2].strip())
+            edges.append((v1, v2))
+        elif line.find("c") == 0 :
+            pass
+
+    return Graph(vertex_count, edge_count, edges)
+
+if __name__ == '__main__' :
+    from os import listdir
+    from os.path import isfile, join
+
+    dir_path = '.\\data'
+    files = [join(dir_path, f) for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    graphs = list(map(read_instance, files))
+
+    print (graphs)
+    print (len(graphs))
